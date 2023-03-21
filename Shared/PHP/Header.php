0aab79d2175d77bb7675843eb4975c70b4d43c13
xiang0619 <111667816+xiang0619@users.noreply.github.com>
2023-03-22 00:13:59 +0800

    Update Header and upload stationary page

--- Shared/PHP/Header.php
+++ Shared/PHP/Header.php
@@ -45,18 +45,18 @@
                     <img src="Shared/Image/b0b8203f-68d2-47f6-a397-269a9aa1f618.png" width="110px" height="110px" alt="Logo"/>
                 </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
                     </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                       <li class="nav-item">
-                            <a class="nav-link active fs-5" aria-current="page" href="#">Home</a>
+                            <a class="nav-link fs-5 primary-color" aria-current="page" href="#">Home</a>
                       </li>
                       <li class="nav-item ">
-                            <a class="nav-link fs-5" href="#">Stationery</a>
+                            <a class="nav-link fs-5 primary-color" href="#">Stationery</a>
                       </li>
                       <li class="nav-item">
-                            <a class="nav-link fs-5" href="#">Service</a>
+                            <a class="nav-link fs-5 primary-color" href="#">Service</a>
                       </li>
                     </ul>
                     <form class="d-flex" role="search">
---
+++ Stationary.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+        <link href="Shared/CSS/SharedCSS.css" rel="stylesheet" type="text/css"/>
+        <title>Stationary</title>
+    </head>
+    <body>
+        <?php
+            include dirname(__FILE__).'/Shared/PHP/Header.php';
+        ?>
+        
+        <div class="row m-5">
+            <div class="col-1"></div>
+            <div class="col-2 offcanvas-body">
+                <ul class="list-unstyled">
+                    <li class="m-2"><a href="#" class="fs-5 nav-link primary-color">Pencil</a></li>
+                    <li class="m-2"><a href="#" class="fs-5 nav-link primary-color">Tape</a></li>
+                    <li class="m-2"><a href="#" class="fs-5 nav-link primary-color">Pen</a></li>
+                    <li class="m-2"><a href="#" class="fs-5 nav-link primary-color">Erase</a></li>
+                    <li class="m-2"><a href="#" class="fs-5 nav-link primary-color">Paper</a></li>
+                    <li class="m-2"><a href="#" class="fs-5 nav-link primary-color">Note Book</a></li>
+                    <li class="m-2"><a href="#" class="fs-5 nav-link primary-color">Scissors</a></li>
+                </ul>
+            </div>
+            <div class="col-8 d-flex">
+                
+                <div class="row">
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/tape1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Yellow Tape</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/tape2.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">White Tape</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/tape3.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Black Tape</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/tape4.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">White Tape</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/pencil1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Yellow Pencil</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/pencil2.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Red Pencil</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/pencil3.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Black Pencil</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/pencil4.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Blue Pencil</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col">
+                        <div class="card m-2" style="width: 16rem; height:30rem">
+                            <img src="Shared/Image/pencil4.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Blue Pencil</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col w-100 mt-3">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-end">
+                            <li class="page-item"><a class="page-link primary-color" href="#">Previous</a></li>
+                            <li class="page-item"><a class="page-link primary-color" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link primary-color" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link primary-color" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link primary-color" href="#">Next</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                </div>
+            </div>
+            
+            <div class="col-1"></div>
+        </div>
+        
+        <?php
+            include dirname(__FILE__).'/Shared/PHP/Footer.php';
+        ?>
+    </body>
+</html>
