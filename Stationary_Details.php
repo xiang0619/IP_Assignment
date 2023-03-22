71c7d473b8575b5e15cfe2314a0fe563f3f4195a
xiang0619 <111667816+xiang0619@users.noreply.github.com>
2023-03-22 23:34:23 +0800

    Update Stationary Details page

--- Stationary_Details.php
+++ Stationary_Details.php
@@ -21,14 +21,19 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                         <img src="Shared/Image/tape1.jpg" class="rounded float-start w-100 shadow" alt=""/>
                     </div>
                     <div class="col-7">
-                        <div class="fs-2 primary-color">
+                        <div class="fs-3 primary-color">
                             Blue Tape
                         </div>
                         <br>
-                        <div class="fs-3 primary-color">
+                        <div class="fs-4 primary-color">
                             RM 10.00
                         </div>
-                        <div class="fs-4 primary-color mt-4">
+                        <div class="fs-4 primary-color mt-2">
+                            <label for="quantity">Quantity : </label>
+                            <input class="ms-2" type="number" value="1" id="quantity" name="quantity" min="1" max="20">
+                            <p class="fs-5">20 piece Available</p>
+                        </div>
+                        <div class="fs-5 primary-color mt-4">
                             <a href="#" class="primary-color shadow p-2 rounded">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill icon-size" viewBox="0 0 16 16">
                                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
