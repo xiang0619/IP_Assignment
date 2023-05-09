9aea832c4728d95087ce6bf879c86950685303a8
xiang0619 <111667816+xiang0619@users.noreply.github.com>
2023-05-09 12:21:39 +0800

    Admin Profile Function Done

--- Admin/AdminChangePassword.php
+++ Admin/AdminChangePassword.php
@@ -4,8 +4,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to c
 Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
 -->
 <html>
-    <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>JE Admin</title>
 	
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -32,15 +33,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         </style>
     </head>
     <body>
-        <?php
-            require '../Shared/Database/StaffDatabase.php';
-            //到时候就是从session那边那data
-            //todo: Ng Wen Xiang get id from session
-            
-            $staffDatabase = new StaffDatabase();
-            $staff = $staffDatabase->getProfile("jcyMiBKoHb1EYUTrTg+QdsFySTEE0EjVFf+ae5stzQA=");
-        ?>
-        
         <div class="sticky-top">
         <nav class="navbar navbar-dark bg-dark sticky-top">
           <div class="container-fluid">           
@@ -129,43 +121,41 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         </nav>
         </div>
         
-        <form action="../Validation/AdminEditProfile.php" method="post">
-        <div class="row">
-            
-            <!-- Make div center -->
-            <div class="col-3"></div>
-            
-            <div class="col-6 shadow-lg text-center m-5">
-                <div class="row">
-                    <!-- Icon position -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-people-fill col-12 m-0" viewBox="0 0 16 16">
-                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                    </svg>
+         <form action="../Validation/AdminChangePassword.php" method="post">
+            <div class="row">
+                <!-- Make div center -->
+                <div class="col-3"></div>
 
-                    <div class="col-12 fs-3">Profile</div>
-                    
-                    <div class="col-4 fs-5 mt-2 mb-2">Email : </div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><?php echo $staff->getEmail(); ?></div>
-                    
-                    <div class="col-4 fs-5 mt-2 mb-2">Position : </div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><?php echo $staff->getPosition(); ?></div>
-                    
-                    <div class="col-4 fs-5 mt-2 mb-2">Name : </div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><input type="text" name="name" class="form-control" id="name" value="<?php echo $staff->getName() ?>" maxlength="40"></div>
+                <div class="col-6 shadow-lg text-center m-5">
+                    <div class="row">
+                        <!-- Icon position -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-shield-lock-fill col-12 m-0 mt-4" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z"/>
+                        </svg>
 
-                    <div class="col-4 fs-5 mt-2 mb-2"><label for="mobile">Mobile Number : </label></div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><input type="text" name="mobile" class="form-control" id="mobile" value="<?php echo $staff->getMobile()?>" placeholder="012-345678910" maxlength="40"></div>
-                    
-                    <div class="col-4 mt-2 mb-2"><button class="form-control" style="border-radius: 25px;background-color: none;"><a style="text-decoration: none;color:black;" href="AdminProfile.php">Back To Profile</a></div>
-                    <div class="col-4 mt-2 mb-2"></div>
-                    <div class="col-4 mt-2 mb-2 "><input type="Submit" class="form-control" name="submit" style=" border-radius: 25px;background-color: none;" value="Edit"></div>
+                        <div class="col-12 fs-3">Change Password</div>
+
+                        <div class="col-4 fs-5 mt-2 mb-2"><label for="oldPassword">Old Password : </label></div>
+                        <div class="col-8 fs-5 mt-2 mb-2"><input name="oldPass" type="password" class="form-control" id="oldPassword" placeholder="**********" maxlength="40" required></div>
+
+                        <div class="col-4 fs-5 mt-2 mb-2"><label for="newPassword">New Password : </label></div>
+                        <div class="col-8 fs-5 mt-2 mb-2"><input name="newPass" type="password" class="form-control" id="newPassword" placeholder="**********" maxlength="40" required></div>
+
+                        <div class="col-4 fs-5 mt-2 mb-2"><label for="rePassword">Confirm Password : </label></div>
+                        <div class="col-8 fs-5 mt-2 mb-2"><input name="rePass" type="password" class="form-control" id="rePassword" placeholder="**********" maxlength="40" required></div>
+
+                        <!--Edit button position  -->
+                        <div class="col-4 mt-2 mb-2"><button class="form-control" style="border-radius: 25px;background-color: none;"><a style="text-decoration: none;color:black;" href="AdminProfile.php">Back To Profile</a></div>
+                        <div class="col-4 mt-2 mb-2"></div>
+                        <div class="col-4 mt-2 mb-2 "><input type="Submit" class="form-control" style="border-radius: 25px;background-color: none;" name="submit" value="Change Password"></div>
+                    </div>
                 </div>
+
+                <!-- Make div center -->
+                <div class="col-3"></div>
+
             </div>
-            
-            <!-- Make div center -->
-            <div class="col-3"></div>
-            
-        </div>
         </form>
+        
     </body>
 </html>
--- Validation/AdminEditProfile.php
+++ Validation/AdminEditProfile.php
@@ -1,23 +1,25 @@
 <?php
 
-require '../Shared/Database/CustomerDatabase.php';
+require '../Shared/Database/StaffDatabase.php';
 
 if(isset($_POST['submit'])){
     
     //到时候就是从session那边那data
     //todo: Ng Wen Xiang get id from session            
-    $customerDatabase = new CustomerDatabase();
-    $encryptionHelper = new EncryptionHelper("Customer");
-    $customer = $customerDatabase->getProfile("YC+rP86LEkvnSmbNZnPq0rG2o2ndUe5V3iSkBQ1Gvd8=");
+    $staffDatabase = new StaffDatabase();
+    $encryptionHelper = new EncryptionHelper("Staff");
+    $staff = $staffDatabase->getProfile("jcyMiBKoHb1EYUTrTg+QdsFySTEE0EjVFf+ae5stzQA=");
     
     $name = isset($_POST['name']) && !empty($_POST['name']) ? $_POST['name'] : null;
     $mobile = isset($_POST['mobile']) && !empty($_POST['mobile']) ? $_POST['mobile'] : null;
+    
+    print_r($staff);
     
     if($name == null){
         
         echo '<script>';
         echo 'alert("Please enter name!!");';
-        echo 'window.location.href = "http://localhost/IP_Assignment/Customer/EditProfile.php";';
+        echo 'window.location.href = "http://localhost/IP_Assignment/Admin/AdminEditProfile.php";';
         echo '</script>';
         exit();
     }
@@ -26,21 +28,21 @@ if(isset($_POST['submit'])){
         
         echo '<script>';
         echo 'alert("Please mobile number!!");';
-        echo 'window.location.href = "http://localhost/IP_Assignment/Customer/EditProfile.php";';
+        echo 'window.location.href = "http://localhost/IP_Assignment/Admin/AdminEditProfile.php";';
         echo '</script>';
         exit();
     }
     
     if($name != null && $mobile != null){
         
-        $customer->setName($name);
-        $customer->setMobile($mobile);
+        $staff->setName($name);
+        $staff->setMobile($mobile);
         
-        $customerDatabase->updateProfile($customer);
+        $staffDatabase->updateProfile($staff);
         
         echo '<script>';
         echo 'alert("Profile Updated.");';
-        echo 'window.location.href = "http://localhost/IP_Assignment/Customer/Profile.php";';
+        echo 'window.location.href = "http://localhost/IP_Assignment/Admin/AdminProfile.php";';
         echo '</script>';
         exit();
     }
