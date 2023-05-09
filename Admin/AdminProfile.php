4704e19a55abca15ea01f42e8d0a2332574e10ce
xiang0619 <111667816+xiang0619@users.noreply.github.com>
2023-05-09 20:07:09 +0800

    profile add xml

--- Admin/AdminProfile.php
+++ Admin/AdminProfile.php
@@ -1,8 +1,38 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
--->
+<?php
+$xslFile = "../Admin/xsl/AdminProfile.xsl";
+
+require '../Shared/Database/StaffDatabase.php';
+//到时候就是从session那边那data
+//todo: Ng Wen Xiang get id from session
+            
+$staffDatabase = new StaffDatabase();
+$staff = $staffDatabase->getProfile("jcyMiBKoHb1EYUTrTg+QdsFySTEE0EjVFf+ae5stzQA=");
+
+$xml = new SimpleXMLElement('<Staff/>');
+
+$profile = $xml->addChild('Staff');
+
+$profile->addChild('id', $staff->getID());
+$profile->addChild('email', $staff->getEmail());
+$profile->addChild('name', $staff->getName());
+$profile->addChild('mobile', $staff->getMobile());
+$profile->addChild('password', $staff->getPassword());
+$profile->addChild('status', $staff->getStatus());
+$profile->addChild('position', $staff->getPosition());
+$profile->addChild('updatedID', $staff->getUpdatedID());
+$profile->addChild('updatedDate', $staff->getUpdatedDate());
+$profile->addChild('createdID', $staff->getCreatedID());
+$profile->addChild('createdDate', $staff->getCreatedDate());
+
+// Apply the XSLT stylesheet to the XML data
+$xslt = new XSLTProcessor();
+$xsldoc = new DOMDocument();
+$xsldoc->load($xslFile);
+$xslt->importStylesheet($xsldoc);
+$html = $xslt->transformToXML($xml);
+
+?>
 <html>
     <head>
 	<meta charset="UTF-8">
@@ -33,14 +63,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         </style>
     </head>
     <body>
-        <?php
-            require '../Shared/Database/StaffDatabase.php';
-            //到时候就是从session那边那data
-            //todo: Ng Wen Xiang get id from session
-            
-            $staffDatabase = new StaffDatabase();
-            $staff = $staffDatabase->getProfile("jcyMiBKoHb1EYUTrTg+QdsFySTEE0EjVFf+ae5stzQA=");
-        ?>
         
         <div class="sticky-top">
         <nav class="navbar navbar-dark bg-dark sticky-top">
@@ -135,42 +157,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         </div>
         <!-- Main Content Area -->
         
-        <div class="row">
-            
-            <!-- Make div center -->
-            <div class="col-3"></div>
-            
-            <div class="col-6 shadow-lg text-center m-5">
-                <div class="row">
-                    <!-- Icon position -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-people-fill col-12 m-0" viewBox="0 0 16 16">
-                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                    </svg>
-
-                    <div class="col-12 fs-3">Profile</div>
-                    
-                    <div class="col-4 fs-5 mt-2 mb-2">Email : </div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><?php echo $staff->getEmail(); ?></div>
-                    
-                    <div class="col-4 fs-5 mt-2 mb-2">Position : </div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><?php echo $staff->getPosition(); ?></div>
-                    
-                    <div class="col-4 fs-5 mt-2 mb-2">Name : </div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><?php echo $staff->getName(); ?></div>
-                    
-                    <div class="col-4 fs-5 mt-2 mb-2">Mobile Number : </div>
-                    <div class="col-8 fs-5 mt-2 mb-2"><?php echo $staff->getMobile(); ?></div>
-                    
-                    <div class="col-4 mt-2 mb-2"><a href="AdminChangePassword.php" class="form-control" style="border-radius: 25px;background-color: none; ">Change Password</a></div>
-                    <div class="col-4 mt-2 mb-2"></div>
-                    <div class="col-4 mt-2 mb-2 "><a href="AdminEditProfile.php" class="form-control" style="border-radius: 25px;background-color: none;">Edit Profile</a></div>
-                </div>
-            </div>
-            
-            <!-- Make div center -->
-            <div class="col-3"></div>
-            
-        </div>
+        <?php
+            echo $html;
+        ?>
         
     </body>
 </html>
