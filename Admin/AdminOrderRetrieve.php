c5148b2ea8ce43f3523ee35973ad7ed2483cbe81
KahSeng <chinks-pm20@student.tarc.edu.my>
2023-05-12 19:29:33 +0800

    finished admin order

---
+++ Admin/AdminOrderRetrieve.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../Shared/DesignPattern/AdminProductFacade.php';
+require_once '../Shared/DesignPattern/AdminServiceFacade.php';
+
+$host = "localhost";
+$dbname = "ip";
+$user = "root";
+$password = "";
+
+$dsn = "mysql:host=$host;dbname=$dbname"; //dsn=database source name
+
+$pdo = new PDO($dsn,$user,$password);//connect to MYSQL using PDO class
+$facade = new AdminProductFacade($pdo);
+$facadeS = new AdminServiceFacade($pdo);
+$orderDetails = $facade->retrieveAllOrders();
+
+// Convert the data to XML format
+$xml = new SimpleXMLElement('<orders/>');
+
+foreach ($orderDetails as $orderDetail){
+    $customerName = $facadeS->retrieveCustomerName($orderDetail['customerID']);
+    $order = $xml->addChild('order');
+    $order->addChild('paymentID', $orderDetail['paymentID']);
+    $order->addChild('customerID', $orderDetail['customerID']);
+    $order->addChild('paymentMethod', $orderDetail['paymentMethod']);
+    $order->addChild('totalPayment', $orderDetail['totalPayment']);
+    $order->addChild('orderID', $orderDetail['orderID']);
+    $order->addChild('customerName', $customerName['customerName']);    
+}
+?>
